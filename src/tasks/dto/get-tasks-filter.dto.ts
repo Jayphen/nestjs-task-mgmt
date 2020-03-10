ac75dab2efc3b7f0a3804b93cc882b9f2823c287
Jayphen <software@jayphen.com>
2020-03-10 18:19:14 +0100

    Set up auth boilerplate

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -3,7 +3,7 @@ import { IsOptional, IsIn, IsNotEmpty } from 'class-validator';
 
 export class GetTasksFilterDto {
   @IsOptional()
-  @IsIn([TaskStatus.OPEN, TaskStatus.DONE, TaskStatus.IN_PROGRESS])
+  @IsIn(Object.keys(TaskStatus))
   status: TaskStatus;
 
   @IsOptional()
