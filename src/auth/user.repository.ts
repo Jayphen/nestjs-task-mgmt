f186e60c7d11891806e1d45ffeff2a20ad120357
Jayphen <software@jayphen.com>
2020-03-10 19:36:47 +0100

    Add signup dto

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,5 +1,16 @@
 import { Repository, EntityRepository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @EntityRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  async signup(authCrendentialsDto: AuthCredentialsDto) {
+    const { password, username } = authCrendentialsDto;
+
+    const user = new User();
+
+    user.username = username;
+    user.password = password;
+    await user.save();
+  }
+}
