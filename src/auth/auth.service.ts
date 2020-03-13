13dae4557e06971bd861be841e39aa292144fca6
Jayphen <software@jayphen.com>
2020-03-13 19:40:25 +0100

    Add basic logging

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, UnauthorizedException, Logger } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
@@ -7,6 +7,8 @@ import { JwtPayload } from 'src/auth/jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
+  private logger = new Logger('AuthService');
+
   constructor(
     @InjectRepository(UserRepository)
     private userRepository: UserRepository,
@@ -29,6 +31,7 @@ export class AuthService {
     const payload: JwtPayload = { username };
 
     const accessToken = await this.jwtService.signAsync(payload);
+    this.logger.debug(`Generated JWT with payload ${JSON.stringify(payload)}`);
 
     return { accessToken };
   }
